b5c98d8c9e70c328cdf8714b6649052dcd759f56
rawsk <rawski.andrzej@gmail.com>
2020-03-04 10:30:55 +0100

    Added CRUD Methods for CustomerController

--- src/main/java/pl/antygravity/zooShop/CustomerController.java
+++ src/main/java/pl/antygravity/zooShop/CustomerController.java
@@ -1,17 +1,55 @@
 package pl.antygravity.zooShop;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequestMapping("/customers")
 @RestController
 public class CustomerController {
 
     @Autowired
-    private CustomerRepository customerRepository;
+    CustomerRepository customerRepository;
 
-    @GetMapping("/customers")
-    public Iterable<Customer> getCustomers (){
-        return customerRepository.findAll();
+    @GetMapping("")
+    public Iterable<Customer> getCustomers(@RequestParam(value = "name", required = false) String name) {
+        if (name == null) {
+            return customerRepository.findAll();
+        } else {
+            Customer byName = customerRepository.findByName(name);
+            List<Customer> customerFoundByName = new ArrayList<>();
+            customerFoundByName.add(byName);
+            return customerFoundByName;
+        }
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Customer> getCustomerById(@PathVariable("id") Long id){
+        return customerRepository.findById(id);
+    }
+
+    @PostMapping("")
+    public Customer createNewCustomer(@RequestBody Customer customer){
+        return customerRepository.save(customer);
+    }
+
+    @DeleteMapping("")
+    private void deleteCustomer(@RequestParam("name") String name){
+        customerRepository.delete(customerRepository.findByName(name));
+    }
+
+    @PutMapping("/{id}")
+    public void updateCustomer(@PathVariable("id") Long id, @RequestBody Customer customer){
+
+        Optional<Customer> byId = customerRepository.findById(id);
+        if (byId.isPresent()){
+            byId.get().setName(customer.getName());
+            byId.get().setEmailAddress(customer.getEmailAddress());
+            byId.get().setPhoneNumber(customer.getPhoneNumber());
+            customerRepository.save(byId.get());
+        }
     }
 }
